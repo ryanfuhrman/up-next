477c16837b94dddc1f7e66e222f33a8cd9de3f43
ryanfuhrman <rsfuhrman10@gmail.com>
2021-03-02 16:13:55 -0500

    Added Lists page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,20 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav>
       <h1>Up Next</h1>
       <ul>
-        <li>Lists</li>
-        <li>Account</li>
+        <li>
+          <Link href="/lists">
+            <a>Lists</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>Account</a>
+          </Link>
+        </li>
       </ul>
     </nav>
   );
---
+++ pages/lists.js
@@ -0,0 +1,11 @@
+export default function Lists() {
+  return (
+    <>
+      <h1>Your Lists</h1>
+      <ul>
+        <li>List 1</li>
+        <li>List 2</li>
+      </ul>
+    </>
+  );
+}
